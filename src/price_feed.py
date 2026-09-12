c396bb621c75bd4028c95847d4d53aa21d09a7dc
Komodoskw <komodosingkawang@gmail.com>
2026-09-13 01:40:50 +0700

    Implement get_gold_data function for gold prices
    Added function to fetch gold price data from Yahoo Finance.

--- src/price_feed.py
+++ src/price_feed.py
@@ -1,1 +1,9 @@
+import yfinance as yf
+import pandas as pd
 
+def get_gold_data(symbol="XAUUSD=X", interval="5m", period="1d"):
+    data = yf.download(tickers=symbol, interval=interval, period=period)
+    df = pd.DataFrame(data)
+    df.reset_index(inplace=True)
+    df.rename(columns={"Close":"close"}, inplace=True)
+    return df
